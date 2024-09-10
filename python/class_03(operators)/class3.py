9ec6423f79b1cd2e9d7160f825f2579adb23c7e8
ijaz0007 <133153581+ijaz0007@users.noreply.github.com>
2024-09-10 08:06:03 +0400

    Delete python directory

--- python/class_03(operators)/class3.py
+++
@@ -1,7 +0,0 @@
-a : int = 7
-b : int = 2
-
-print(a + b) # addtition
-print(a - b) # subtraction
-print(a * b)
-print(a / b) # division
--- python/class_04(list_its_methods)/class4.py
+++
@@ -1,6 +0,0 @@
-
-# ->                    0        1         2
-names : list[str] = ["Qasim","Sir Zia", "Sir Inam"]
-# <-                    -3     -2          -1
-
-print(names[-2])
